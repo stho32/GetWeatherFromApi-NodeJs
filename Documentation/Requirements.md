fca52351cf45e9e233a492a51fff93285aaef0c3
Stefan Hoffmann <sthoffmann@onlinehome.de>
2018-02-06 06:24:48 +0100

    command line works as expected now

--- Documentation/Requirements.md
+++ Documentation/Requirements.md
@@ -2,4 +2,4 @@
 
   - [x] use nodejs 
   - [x] typing `node app.js 5328041` in your console you should get the current temperature of Beverly Hills
-  - [ ] typing `node app.js Cleaveland OH` in your console you should get the current temperature of Cleaveland, Ohio
+  - [x] typing `node app.js Cleveland US` in your console you should get the current temperature of Cleveland, a city in the US. Actually there are multiple.. but we do not care.
--- Source/app.js
+++ Source/app.js
@@ -21,36 +21,10 @@ if (process.argv.length == 3) {
     cityId = process.argv[2];
 
     loadApiKey(apiKey => {
-        try {
-            http.get("http://api.openweathermap.org/data/2.5/weather?id=" + cityId + "&units=metric&appid=" + apiKey,
-                response => {
-                    let weatherJson = "";
-
-                    response.on("data", data => {
-                        weatherJson += data.toString();
-                    });
-
-                    response.on("end", () => {
-                        let weatherForecast = JSON.parse(weatherJson);
-
-                        if ( weatherForecast.cod == 200 ) {
-                            console.log(`${weatherForecast.name} : ${weatherForecast.main.temp} degree Celsius, humidity: ${weatherForecast.main.humidity}`);
-                        } else if ( weatherForecast.cod == 404 ) {
-                            console.log(weatherForecast.message);
-                        } else {
-                            console.log("Got unknown response from weatherApi: ");
-                            console.log(weatherJson);
-                        }
-                    });
-
-                    response.on("error", error => {
-                        console.log("There has been an error processing the request :" + error.message);
-                    });
-                }
-            );
-        } catch (error) {
-            console.log("There has been an error processing the request :" + error.message);
-        }
+        let url = "http://api.openweathermap.org/data/2.5/weather?id=" + 
+            encodeURIComponent(cityId) + 
+            "&appid=" + encodeURIComponent(apiKey);
+        loadWeatherInformation(url);
     });
 }
 
@@ -60,21 +34,11 @@ if (process.argv.length == 4) {
     countryCode = process.argv[3];
 
     loadApiKey(apiKey => {
-        http.get("http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=" + apiKey,
-            response => {
-                let weatherJson = "";
-
-                response.on("data", data => {
-                    weatherJson += data.toString();
-                });
-
-                response.on("end", () => {
-                    let weatherForecast = JSON.parse(weatherJson);
-
-                    console.dir(weatherForecast);
-                });
-            }
-        );
+        let url = "http://api.openweathermap.org/data/2.5/weather?q=" + 
+            encodeURIComponent(cityName) + "," + 
+            encodeURIComponent(countryCode) + 
+            "&appid=" + encodeURIComponent(apiKey);
+        loadWeatherInformation(url);
     });
 }
 
@@ -91,4 +55,35 @@ function loadApiKey(apiKeyConsumerCallback) {
     });
 }
 
+function loadWeatherInformation(url) {
+    try {
+        http.get(url + "&units=metric",
+            response => {
+                let weatherJson = "";
 
+                response.on("data", data => {
+                    weatherJson += data.toString();
+                });
+
+                response.on("end", () => {
+                    let weatherForecast = JSON.parse(weatherJson);
+
+                    if ( weatherForecast.cod == 200 ) {
+                        console.log(`${weatherForecast.name} : ${weatherForecast.main.temp} degree Celsius, humidity: ${weatherForecast.main.humidity}`);
+                    } else if ( weatherForecast.cod == 404 ) {
+                        console.log(weatherForecast.message);
+                    } else {
+                        console.log("Got unknown response from weatherApi: ");
+                        console.log(weatherJson);
+                    }
+                });
+
+                response.on("error", error => {
+                    console.log("There has been an error processing the request :" + error.message);
+                });
+            }
+        );
+    } catch (error) {
+        console.log("There has been an error processing the request :" + error.message);
+    }
+}
